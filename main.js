b19aa2007f5715ece80657a0c68124dfbbb18cea
Ani67rt <88225238+Ani67rt@users.noreply.github.com>
2022-07-15 20:05:09 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-canvas = document.getElemntById('myCanvas')
+canvas = document.getElementById('myCanvas')
 ctx = canvas.getContext("2d")
 
 img_width = 300;
@@ -9,15 +9,13 @@ var img_image;
 img_x = 100;
 img_y = 100;
 
-function add() {
-	greencar_imgTag = newImage();
-	greencar_imgTag.onload = uploadimg;
-	greencar_imgTag.src = green_image; 
-
-	background_imgTag = newImage();
-  background_imgTag.onload = uploadimg;
- background_imgTag.src = green_image; 
- }
+function add() { img_imgTag = new Image(); 
+	//defining a variable with a new image 
+	img_imgTag.onload = uploadimg; 
+	// setting a function, onloading this variable 
+	img_imgTag.src = img_image; 
+	// load image 
+}
  
 
 
@@ -33,62 +31,23 @@ function my_keydown(e)
 	keyPressed = e.keyCode;
 	console.log(keyPressed);
 	
-		if((keyPressed >=97 && keyPressed<=122)|| (keyPressed >=65 && keyPressed<=90))
+	if((keyPressed >=97 && keyPressed<=122)|| (keyPressed >=65 && keyPressed<=90)) { aplhabetkey();
+		 document.getElementById("d1").innerHTML="You pressed Alphabet Key"; console.log("alphabet key");
+		 } else if(keyPressed >=48 && keyPressed<=57) { numberkey();
+			 document.getElementById("d1").innerHTML="You pressed Number Key";
+			  console.log("Number key"); } else if(keyPressed >=37 && keyPressed<=40) { arrowkey();
+				 document.getElementById("d1").innerHTML="You pressed Arrow Key"; console.log("Arrow Key");
+				 } else if((keyPressed ==17)|| (keyPressed ==18 || keyPressed ==27)) { specialkey();
+					 document.getElementById("d1").innerHTML="You pressed ctrl/esc/alt"; console.log("special key");
+					 } else{ otherkey(); document.getElementById("d1").innerHTML="You pressed symbol or other key";
+					 } 
+					} function aplhabetkey() { img_image="Alpkey.png"; 
+					 add(); 
+					} function numberkey() { img_image="numkey.png";
+					  add(); } function arrowkey() { img_image="Arrkey.png"; 
+					  add(); } function specialkey() { img_image="spkey.png"; 
+					  add(); } function otherkey() { img_image="otherkey.png"; 
+					  add(); }
 		
-	{
-		aplhabetkey();
-		otherkey();
-		document.getElementById("d1").innerHTML="You placed Alphabet key";
-		console.log("alphabet Key")
-	}
-	else if(keyPressed >=48 && keyPressed<=57)
-	{
-     numberkey();
-	 document.getElementById("d1").innerHTML="You pressed Number key"
-	 console.log("Number Key")
-    }
-	else if(keyPressed >=37 && keyPressed<=40)
-	{
-      arrowkey();
-	  document.getElementById("d1").innerHTML="You pressed Arrow key"
-	  console.log("Arrow Key")
-    }
-	else if("keyPressed ==17")  (keyPressed ==18 || keyPressed ==27)
-	{
-      specialkey();
-	  document.getElemntById("d1").innerHTML="You pressed ctrl/esc/alt";
-	  console.log("Special Key")
-    }
-	else if(keyPressed >=20 && keyPressed<)
-	{
-		specialkey();
-		document.getElemntById("d1").innerHTML="You pressed ctrl/esc/alt";
-		console.log("Special Key")
-	  }
-}
-
-function aplhabetkey()
-{
-	img_image="Alpkey.png"
-	add();
-}
-function numberkey()
-{
-	img_image="numkey.png"
-	add();
-}
-function arrowkey()
-{
-	img_image="Arrkey.png"
-	add();
-}
-function specialkey()
-{
-	img_image="spkey.png"
-}
-function otherkey()
-{
-	img_image="otherkey.png";
-	add();
-}
 	
+
